d44d43b6a84d275b9a5d69473e47130538e0310d
iOSNerd <boch2436@gmail.com>
2014-12-22 14:57:31 +0800

    fdsa

---
+++ Pods/Target Support Files/Pods-MicroCommunity-MBProgressHUD/Pods-MicroCommunity-MBProgressHUD-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MicroCommunity_MBProgressHUD : NSObject
+@end
+@implementation PodsDummy_Pods_MicroCommunity_MBProgressHUD
+@end
---
+++ Pods/Target Support Files/Pods-MicroCommunity-SWTableViewCell/Pods-MicroCommunity-SWTableViewCell-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MicroCommunity_SWTableViewCell : NSObject
+@end
+@implementation PodsDummy_Pods_MicroCommunity_SWTableViewCell
+@end
